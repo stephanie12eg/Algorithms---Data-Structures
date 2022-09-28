feaae7a3f7c06a72092bc484b8d8ad7ffdcf21d0
Stephanie Estrada Garcia <stephanieestradagarcia@Stephanies-MacBook-Air.local>
2022-09-28 19:49:24 +0100

    Added a description of what each python code is solving

--- Easy/14-longestcommonprefix.py
+++ Easy/14-longestcommonprefix.py
@@ -1,3 +1,8 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string ""
+# Example 1: Input: strs = ["flower","flow","flight"] Output: "fl"
+# Example 2: Input: strs = ["car","cax","care"] Output: "ca"
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
