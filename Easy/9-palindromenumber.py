093ff43496fefff9298843bb223b73c638866b28
Stephanie Estrada Garcia <stephanieestradagarcia@stephanies-air.home>
2022-09-28 13:52:25 +0100

    second easy leetcode challenge completed with commentary

---
+++ Easy/9-palindromenumber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # the input number is converted into a string, then the an equality boolean operator (returning an output of false or true), 
+        # is used to check whether the original string and reverse string are equal
+        # negative one means that the list is reversed starting from the last element in the string
+        return str(x)==str(x)[::-1]
+        
