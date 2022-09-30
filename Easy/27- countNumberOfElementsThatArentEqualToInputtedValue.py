027e36eff08aa135b5b2618a10abbd0775e88098
Stephanie Estrada Garcia <stephanieestradagarcia@Stephanies-MacBook-Air.local>
2022-09-30 23:44:53 +0100

    seventh easy leetcode challenge completed using the same logic as leetcode challenge 26

---
+++ Easy/27- countNumberOfElementsThatArentEqualToInputtedValue.py
@@ -0,0 +1,24 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+# The relative order of the elements may be changed.
+# if there are k elements after removing the duplicates, 
+# then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+# example: Input: nums = [0,1,2,2,3,0,4,2], val = 2 
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        counter = 0 
+        
+        for i in range (len(nums)):
+            
+            if nums[i] != val:
+                nums[counter] = nums[i]
+                counter +=1
+                
+        return counter 
