294cf5edbd31514fecb2b96c2306abf8ce7a7e42
Stephanie Estrada Garcia <stephanieestradagarcia@stephanies-air.home>
2022-10-01 18:02:00 +0100

    fixing a typo

--- Easy/27- countNumberOfElementsThatArentEqualToInputtedValue.py
+++ Easy/27- countNumberOfElementsThatArentEqualToInputtedValue.py
@@ -3,7 +3,7 @@
 # if there are k elements after removing the duplicates, 
 # then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 # example: Input: nums = [0,1,2,2,3,0,4,2], val = 2 
-# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Output: 5, nums = [0,1,3,0,4,_,_,_]
 
 class Solution(object):
     def removeElement(self, nums, val):
